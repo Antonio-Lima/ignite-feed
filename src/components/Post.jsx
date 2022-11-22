8936baa3c51d69ebf343a4c635675617216c6711
AntonioLima <gutto1998@gmail.com>
2022-11-22 11:34:48 -0300

    Novos componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post} >
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/diego3g.png" alt="" />
+          <Avatar src="https://github.com/diego3g.png" alt="" />
           <div className={styles.authorInfo}>
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
